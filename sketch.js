8220685030c5538420d969acff1faa29c35a94db
NightShadeLily <144826874+NightShadeLily@users.noreply.github.com>
2023-11-06 12:00:09 -0500

    Fixed KeyPressed

--- sketch.js
+++ sketch.js
@@ -29,10 +29,9 @@ function mousePressed() {
 }
 
 function draw() {
-  if(paused){
+  if (paused){
 
   } else {
-
   background(51);
 
   if (s.eat(food)) {
@@ -45,26 +44,22 @@ function draw() {
 
   fill(255, 0, 100);
   rect(food.x, food.y, scl, scl);
-}
+  }
 }
 
 
 
 
 function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    s.dir(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
-    s.dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1, 0);
+  if (key === 'p') {
+    if (keyCode === UP_ARROW) {
+      s.dir(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+      s.dir(0, 1);
+    } else if (keyCode === RIGHT_ARROW) {
+      s.dir(1, 0);
+    } else if (keyCode === LEFT_ARROW) {
+      s.dir(-1, 0);
+    }
   }
 }
-
-function keyPressed(){
-  if(key === 'p'){ 
-      paused = true; 
-   }
-  }
